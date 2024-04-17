666d2567daff69daae18a1fa57eae708869e7d30
Quentin Berling <quentin.berling@epfl.ch>
2024-04-17 12:02:41 +0200

    Improve code quality.

--- Lab2/task4.py
+++ Lab2/task4.py
@@ -6,10 +6,12 @@ Created on Thu Apr 11 15:11:31 2024
 @author: quentin
 """
 
+## Imports
 import copy
 import random
 import pandas as pd
 
+## Initial arguments
 maxsteps=10
 
 courses=["MT101", "MT102", "MT103", "MT104", "MT105", "MT106", "MT107", 
@@ -22,6 +24,7 @@ random.shuffle(courses)
 
 classrooms=pd.DataFrame(dict(TP51=courses[:8], SP34=courses[8:16], K3=courses[16:24]), range(9, 17))
 
+## Functions
 def random_conflict(classrooms):
     conflicts_list=[]
     
@@ -48,30 +51,50 @@ def count_conflicts(classrooms):
     
     return conflicts_num
 
-conflicts=count_conflicts(classrooms)
-
-while maxsteps > 0 and count_conflicts(classrooms) > 0:
-    ## Min-Conflicts
-    classroom_conflict_tuple=random_conflict(classrooms)
-    
-    classroom=classroom_conflict_tuple[0]
-    course_index=classroom_conflict_tuple[1]
-    
-    classrooms_new=copy.deepcopy(classrooms)
+def optimize_conflicts(classrooms, classtuple, index_to_test):
+    conflicts=count_conflicts(classrooms)
+    classroom=classtuple[0]
+    course_index=classtuple[1]
+    classrooms_new=None
     
     for col in classrooms:
-        for i in classrooms.index:
+        for i in index_to_test:
             if i != course_index:
                 classrooms_test=copy.deepcopy(classrooms)
                 classrooms_test.at[i, col]=classrooms.loc[course_index, classroom]
                 classrooms_test.at[course_index, classroom]=classrooms.loc[i, col]
                 
                 conflicts_test=count_conflicts(classrooms_test)
-                if conflicts_test < conflicts:
+                if conflicts_test <= conflicts:
                     classrooms_new=copy.deepcopy(classrooms_test)
                     conflicts=conflicts_test
+    
+    if classrooms_new is not None:
+        return classrooms_new
+    
+    return classrooms
 
-    classrooms=copy.deepcopy(classrooms_new)
+def apply_preferences(classrooms, classtuples, prefs):
+    for classtuple in classtuples:
+        course_index=classtuple[1]
+        if course_index in prefs:
+            prefs.remove(course_index)
+            classtuples.remove(classtuple)
+            
+    for classtuple in classtuples:
+        classrooms=optimize_conflicts(classrooms, classtuple, prefs)
+    
+    return classrooms
+
+
+## Main
+conflicts=count_conflicts(classrooms)
+
+while maxsteps > 0 and count_conflicts(classrooms) > 0:
+    ## Min-Conflicts
+    classroom_conflict_tuple=random_conflict(classrooms)
+    classrooms=optimize_conflicts(classrooms, classroom_conflict_tuple, classrooms.index)
+    
     maxsteps-=1
     
     ## Empty classes
@@ -80,29 +103,8 @@ while maxsteps > 0 and count_conflicts(classrooms) > 0:
     for col in classrooms:
         for i in classrooms[(classrooms[col] == '')].index.tolist():
             classrooms_empty.append((col, i))
-
-    for classtuple in classrooms_empty:
-        classroom=classtuple[0]
-        course_index=classtuple[1]
-        if course_index in classrooms_empty_pref:
-            classrooms_empty_pref.remove(course_index)
-            classrooms_empty.remove(classtuple)
-            
-    for classtuple in classrooms_empty:
-        classroom=classtuple[0]
-        course_index=classtuple[1]
-        for col in classrooms:
-            for i in classrooms_empty_pref:
-                classrooms_test=copy.deepcopy(classrooms)
-                classrooms_test.at[i, col]=classrooms.loc[course_index, classroom]
-                classrooms_test.at[course_index, classroom]=classrooms.loc[i, col]
-                
-                conflicts_test=count_conflicts(classrooms_test)
-                if conflicts_test <= conflicts:
-                    classrooms_new=copy.deepcopy(classrooms_test)
-                    conflicts=conflicts_test
     
-        classrooms=copy.deepcopy(classrooms_new)
+    classrooms=apply_preferences(classrooms, classrooms_empty, classrooms_empty_pref)
     
     ## MT50x
     classrooms_50x=[]
@@ -111,28 +113,6 @@ while maxsteps > 0 and count_conflicts(classrooms) > 0:
         for i in classrooms[(classrooms[col].str.contains('MT5'))].index.tolist():
             classrooms_50x.append((col, i))
     
-    for classtuple in classrooms_50x:
-        classroom=classtuple[0]
-        course_index=classtuple[1]
-        if course_index in classrooms_50x_pref:
-            classrooms_50x_pref.remove(course_index)
-            classrooms_50x.remove(classtuple)
-            
-    for classtuple in classrooms_50x:
-        classroom=classtuple[0]
-        course_index=classtuple[1]
-        for col in classrooms:
-            for i in classrooms_50x_pref:
-                classrooms_test=copy.deepcopy(classrooms)
-                classrooms_test.at[i, col]=classrooms.loc[course_index, classroom]
-                classrooms_test.at[course_index, classroom]=classrooms.loc[i, col]
-                
-                conflicts_test=count_conflicts(classrooms_test)
-                if conflicts_test <= conflicts:
-                    classrooms_new=copy.deepcopy(classrooms_test)
-                    conflicts=conflicts_test
-    
-        classrooms=copy.deepcopy(classrooms_new)
-    
+    classrooms=apply_preferences(classrooms, classrooms_50x, classrooms_50x_pref) 
 
 print(classrooms)
