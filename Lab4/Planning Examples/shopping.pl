033da1ca551d1da23a7b138d03daa8f96fa2a7ed
Quentin Berling <quentin.berling@epfl.ch>
2024-05-07 22:08:01 +0200

    Fix planning examples.

--- Lab4/Planning Examples/shopping.pl
+++ Lab4/Planning Examples/shopping.pl
@@ -1,7 +1,38 @@
-<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML 2.0//EN">
-<html><head>
-<title>301 Moved Permanently</title>
-</head><body>
-<h1>Moved Permanently</h1>
-<p>The document has moved <a href="https://dellacqua.se/education/courses/tnm096/material/labs/shopping.pl">here</a>.</p>
-</body></html>
+%====================================================================
+% SHOPPING PROBLEM
+
+% CONTROL PARAMETERS
+ordering(linear).
+derivedPlans(one).
+
+% ACTIONS:    action(Name,Prec,Del,Add)
+
+% Agent A buys item X at the Store
+action(buy(A,W,Store),
+	[store(Store),at(A,Store),sells(Store,W)],
+	[],
+	[has(A,W)]).
+
+% Agent A goes from location X to location Y
+action(go(A,X,Y),
+	[location(X),location(Y),X\=Y,at(A,X)],
+	[at(A,X)],
+	[at(A,Y)]).
+
+% FLUENT
+fluent(at(_,_)).
+fluent(has(_,_)).
+
+% DOMAIN KNOWLEDGE
+store(ica) <- [].
+store(clasohlson) <- [].
+sells(ica,banana) <- [].
+sells(ica,bread) <- [].
+sells(ica,cheese) <- [].
+sells(clasohlson,drill) <- [].
+location(home) <- [].
+location(office) <- [].
+location(X) <- [store(X)].
+
+% INITIAL SITUATION
+holds(at(chris,home),init).
--- Lab4/Planning Examples/tv.pl
+++ Lab4/Planning Examples/tv.pl
@@ -1,7 +1,46 @@
-<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML 2.0//EN">
-<html><head>
-<title>301 Moved Permanently</title>
-</head><body>
-<h1>Moved Permanently</h1>
-<p>The document has moved <a href="https://dellacqua.se/education/courses/tnm096/material/labs/tv.pl">here</a>.</p>
-</body></html>
+% TV PROBLEM
+
+% CONTROL PARAMETERS
+% Total order over the actions
+% 	ordering(linear).
+% Partial order over the actions
+% 	ordering(partial).
+%
+% 	parallel(A,B).
+% defines which actions can be executed in parallel, that is,
+% there is no order among them
+
+% Given a partial oreder plan, the directive
+% 	derivedPlans(one).
+% makes the planner to derive only one plan.
+% In contrast,
+% 	derivedPlans(all).
+% makes the planner to derive all plans.
+
+ordering(linear).
+derivedPlans(one).
+
+
+% ACTIONS:    action(Name,Prec,Del,Add)
+action(study,[],[],[readyForExam]).
+action(train,[],[],[goodShape,hungry]).
+action(eat,[],[],[sated]).
+action(watchTV,[],[],[watchedTV]).
+
+
+% DOMAIN KNOWLEDGE
+ags253g <- [].
+
+% FLUENT
+fluent(goodShape).
+fluent(badShape).
+fluent(readyForExam).
+fluent(hungry).
+fluent(sated).
+fluent(watchedTV).
+
+
+% INITIAL SITUATION
+holds(badShape,init).
+
+
