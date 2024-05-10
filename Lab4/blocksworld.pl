89b93d08652ae63c2a37382386f0e1f13954e34c
klarabratteby <93648999+klarabratteby@users.noreply.github.com>
2024-05-10 14:12:39 +0200

    Added initial and goal state

--- Lab4/blocksworld.pl
+++ Lab4/blocksworld.pl
@@ -6,3 +6,17 @@ pyramid(X) :- X in 5..6.
 orange(X) :- X in 1 \/ 4.
 green(X) :- X in 2 \/ 5.
 blue(X) :- X in 3 \/ 6.
+
+% Actions
+act(move(X,A,B),[...],[...],[...]):-
+block(X),
+A #\= B.
+
+
+% Goal state
+goal_state([on(X,Y), on(Y,Z)]):-
+green(Y).
+blue(Z).
+
+% Initial state
+initial_state([on(1,0),on(2,0),on(3,4),on(4,0),on(5,6),on(6,0)]).
